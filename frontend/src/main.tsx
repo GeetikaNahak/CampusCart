48018a16cb7e7fd424b8db6e7111074bfa72a6e4
GeetikaNahak <geetikasaisravaninahak@gmail.com>
2025-01-14 21:16:52 +0530

    frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter as Router } from 'react-router-dom'
+import AppRoutes from './AppRoutes.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router></Router>
+    <Router>
+      <AppRoutes/>
+    </Router>
   </StrictMode>,
 )
