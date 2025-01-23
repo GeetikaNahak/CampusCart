e4afe0c718ffffe4ecb39f0e808e9bdce5d2c415
GeetikaNahak <geetikasaisravaninahak@gmail.com>
2025-01-23 21:04:52 +0530

    Main Nav

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,6 +2,7 @@
 
 import { Link } from "react-router-dom"
 import MobileNav from "./MobileNav"
+import MainNav from "./MainNav"
 
 const Header = () => {
   return (
@@ -10,6 +11,9 @@ const Header = () => {
       <Link to='/' className='text-3xl font-bold tracking-tight text-black-500'>CampusCart.com</Link>
       <div className="md:hidden">
         <MobileNav/>
+      </div>
+      <div className="hidden md:block">
+        <MainNav/>
       </div>
     </div>
   )
---
+++ frontend/src/components/MainNav.tsx
@@ -0,0 +1,8 @@
+// import React from 'react'
+
+export default function MainNav() {
+  return (
+    <div>MainNavButton</div>
+  )
+}
+
