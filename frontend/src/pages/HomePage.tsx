a29b4ec4c99193dbb19c10663ee93df01514b33c
GeetikaNahak <geetikasaisravaninahak@gmail.com>
2025-01-30 21:44:11 +0530

    HomePage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,5 +1,6 @@
 // import React from 'react'
-
+import landingImage from '../assets/landingImage.png'
+import appDownload from '../assets/hero.jpg'
 export default function HomePage() {
   return (
     <div className="flex flex-col gap-12">
@@ -8,6 +9,17 @@ export default function HomePage() {
                 Tuck into a takeway today
             </h1>
             <span className="text-xl">Food is just a click away!!</span>
+        </div>
+        <div className="grid md:grid-cols-2 gap-5">
+          <img src={landingImage} alt="" />
+          <div className="flex flex-col items-center justify-center gap-4 text-center">
+            <span className='font-bold text-3xl tracking-tighter'>
+              Order takeaway even faster!
+            </span>
+            <span>
+              Download the App for faster and personalized experience
+            </span>
+          </div>
         </div>
     </div>
   )
