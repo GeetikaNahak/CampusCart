ae008d5fa59f5e3440bc15495d0b6010d377afb5
GeetikaNahak <geetikasaisravaninahak@gmail.com>
2025-02-03 22:32:46 +0530

    Auth0

---
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -0,0 +1,27 @@
+// import { redirect } from "react-router-dom";
+
+import { Auth0Provider } from "@auth0/auth0-react";
+
+// import React from 'react'
+type Props={
+    children:React.ReactNode;
+}
+
+export default function Auth0ProviderWithNavigate({children}:Props) {
+    const domain=import.meta.env.VITE_AUTH0_DOMAIN;
+    const clientId=import.meta.env.VITE_AUTH0_CLIENT_ID;
+    const redirectUri=import.meta.env.VITE_AUTH0_CALLBACK_URL;
+    if(!domain || !clientId || redirectUri){
+        throw new Error("unable to initialize auth");
+    }
+
+  return (
+    <Auth0Provider domain={domain} clientId={clientId} authorizationParams={{
+        redirect_uri:redirectUri,
+    }}>
+
+        {children}
+    </Auth0Provider>
+  )
+}
+
