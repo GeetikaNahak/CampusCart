0998ff27a49f54638820375448750355ec92f0cd
GeetikaNahak <geetikasaisravaninahak@gmail.com>
2025-02-13 22:23:08 +0530

    LandingPage

--- frontend/src/layouts/layouts.tsx
+++ frontend/src/layouts/layouts.tsx
@@ -1,6 +1,6 @@
 // import React from 'react'
 
-import Footer from "@/components/footer";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 type Props={
