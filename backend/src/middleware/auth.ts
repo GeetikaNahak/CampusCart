0cd6f88ef0ceb56911e835b04638713e46be4229
GeetikaNahak <geetikasaisravaninahak@gmail.com>
2025-06-15 02:17:56 +0530

    validate access token

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,7 +1,7 @@
 import { auth } from "express-oauth2-jwt-bearer";
 
-const jwtCheck = auth({
-  audience: 'campus-cart-app-api',
-  issuerBaseURL: 'https://dev-u4cexii23wkuqb3p.us.auth0.com/',
+export const jwtCheck = auth({
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
   tokenSigningAlg: 'RS256'
 });
--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
 import { validateMyUserRequest } from "../middleware/validation";
+import { jwtCheck } from "../middleware/auth";
 
 const router = express.Router();
 
-router.post("/", MyUserController.createCurrentUser);
+router.post("/", jwtCheck, MyUserController.createCurrentUser);
 router.put("/",validateMyUserRequest,MyUserController.updateCurrentUser);
 export default router;
