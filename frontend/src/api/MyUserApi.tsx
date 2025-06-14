5dab7e3b034523e87e6b0d7374e6d1b037c678dc
GeetikaNahak <geetikasaisravaninahak@gmail.com>
2025-06-15 01:00:17 +0530

    Creating user Request

--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -1,6 +1,6 @@
 // import exp from "constants";
 
-import { useAuth0 } from "@auth0/auth0-react";
+// import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation } from "react-query";
 
 const API_BASE_URL= import.meta.env.VITE_API_BASE_URL;
@@ -18,6 +18,7 @@ export const useCreateMyUser=()=>{
                 "Content-Type":"application/json",
             },
             body:JSON.stringify(user),
+            
         })
         if(!response.ok){
             throw new Error("Failed to create User");
@@ -58,6 +59,9 @@ export const useUpdateMyUser=()=>{
     const {mutateAsync:updateUser, isLoading,isSuccess,isError,error,reset}=useMutation(updateMyUserRequest);
 
     return {
-        updateUser,isLoading
+        updateUser,isLoading,
+        isSuccess,
+        isError,
+        error,reset
     }
 }
--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,5 +1,6 @@
 // import { redirect } from "react-router-dom";
 
+import { useCreateMyUser } from "@/api/MyUserApi";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
 
 // import React from 'react'
@@ -9,6 +10,7 @@ type Props={
 
 
 export default function Auth0ProviderWithNavigate({children}:Props) {
+    const {createUser}=useCreateMyUser();
     const domain=import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId=import.meta.env.VITE_AUTH0_CLIENT_ID;
     const redirectUri=import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -16,8 +18,9 @@ export default function Auth0ProviderWithNavigate({children}:Props) {
         throw new Error("unable to initialize auth");
     }
     const onRedirectCallback=(appstate?: AppState, user?:User )=>{
-        console.log("USER",user);
-        console.log(appstate);
+        if(user?.sub&&user?.email){
+            createUser({authId: user.sub,email: user.email})
+        }
     }
   return (
     <Auth0Provider domain={domain} clientId={clientId} authorizationParams={{
