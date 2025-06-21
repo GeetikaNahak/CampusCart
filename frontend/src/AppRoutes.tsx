f5e8d85aaa626b95e7cca567e31c290aa9883c0c
GeetikaNahak <geetikasaisravaninahak@gmail.com>
2025-06-21 12:52:13 +0530

    protected route

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -4,12 +4,16 @@ import Layouts from './layouts/Layouts'
 import HomePage from './pages/HomePage'
 import UserProfilePage from './pages/UserProfilePage'
 import AuthCallbackPage from './pages/AuthCallbackPage'
+import ProtectedRoute from './auth/ProtectedRoute'
 
 const AppRoutes = () => {
   return (
     <Routes>
         <Route path='/' element={<Layouts showHero={true}><HomePage/></Layouts>}/>
+        <Route element={<ProtectedRoute/>}>
         <Route path='/user-profile' element={<Layouts> <UserProfilePage/> </Layouts>}/>
+        </Route>
+        
         <Route path='*' element={<Navigate to="/"/>}/>
         <Route path='/auth-callback' element={<AuthCallbackPage/>}/>
     </Routes>
