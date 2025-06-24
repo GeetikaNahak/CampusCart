1537887ef4280007314ffbe684329d669f798540
GeetikaNahak <geetikasaisravaninahak@gmail.com>
2025-06-24 22:30:22 +0530

    search api

--- backend/src/controllers/StoreController.ts
+++ backend/src/controllers/StoreController.ts
@@ -9,7 +9,7 @@ const searchStore=async (req:Request,res:Response):Promise<any>=>{
         const sortOption=(req.query.sortOption as string)||"lastUpdated";
         const page=parseInt(req.query.page as string)||1;
         let query:any={};
-        query["city"]=new RegExp(city,"i");
+        query["storeName"]=new RegExp(city,"i");
         const cityCheck=await Store.countDocuments(query)
         if(cityCheck===0){
             return res.status(404).json([]);
